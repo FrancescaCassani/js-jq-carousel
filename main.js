b0047a70c6535bcc8f1e513c22c84e34afb08d7c
Francesca <fracassani@gmail.com>
2020-10-27 14:34:09 +0100

    Impostazione funzione click freccine

---
+++ main.js
@@ -0,0 +1,15 @@
+/**Creare uno slider di immagini */
+
+$(document).ready(function () {
+
+    //CLICK AL MOUSE
+    $(".next").click(function () { 
+
+    });
+
+
+    $(".prev").click(function () { 
+
+    });
+
+});
