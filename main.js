99575558c22d8f1f88fe7c62c81ca9422178e5c8
Francesca <fracassani@gmail.com>
2020-10-27 15:32:25 +0100

    Next

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
 
     //CLICK AL MOUSE
     $(".next").click(function () { 
-
+        turn("next");
     });
 
 
     $(".prev").click(function () { 
-
+        turn("prev");
     });
 
 });
@@ -17,7 +17,28 @@ $(document).ready(function () {
 
 //Funzione scorrimento
 function turn(direction) {
-    var activeImg = $(".images img.active");  //seleziono la classe active sulle img
-    var activeCircle = $(".nav i.active");    //seleziono la classe active sulle i
+
+    //Variabili
+    var activeImg = $(".images img.active");  //seleziono la classe active sulle img di images
+    var activeCircle = $(".nav i.active");    //seleziono la classe active sulle i di nav
     
+
+    //Reset: tolgo le classi per poi riassegnarle durante la funzione dedicata
+    activeImg.removeClass("active");
+    activeCircle.removeClass("active");
+
+
+    //Next: riassegno le classi nel momento in cui col click attivo l'evento
+    if(direction === "next") {
+
+        if(activeImg.hasClass("last")) {   //con hasClass controllo se il mio elemento ha un nome di classe specificato.
+            $(".images img.first").addClass("active");
+            $(".nav i.first").addClass("active");
+
+        } else {
+            activeImg.next("img").addClass("active");
+            activeCircle.next("i").addClass("active");
+        }
+    }
+
 }
